a3fc708a36feb6951b89d67d239d2197e8e10d31
chemini <81946660+chemini@users.noreply.github.com>
2021-05-20 13:37:02 +0530

    Add files via upload
    init C++ calculator

---
+++ allfiles.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	cout <<"hello!"<<endl;
+	
+	string fname, lname;
+	
+	cout<<"please enter your first name"<<endl;
+	
+	cin>> fname;
+	
+	cout<<"please enter your last name"<<endl;
+		
+	cin >> lname;
+	
+	cout <<"hello " << fname <<" "<<lname << " you are warmly welcome to programmers hub"<<endl;
+	return 0;
+	
+	system("pause");
+}
---
+++ calculator.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	
+	int sum;
+	
+	cout <<"Enter a number :" <<endl;
+	
+	cin>> a;
+	
+	cout<<"Enter another number :" <<endl;
+	cin >> b;
+	sum = a + b;
+	
+	cout << "The sum of those numbers is :" << sum << endl;
+	
+	return 0;
+	
+	system("pause");
+}
